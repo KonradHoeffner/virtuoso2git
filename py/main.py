52de06b9936a5306e31cd0ae65725d4e2a6ddb21
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-11-08 15:44:27 +0100

    Runs through for some files but discontinued for now because there are often errors that it cannot fix.

--- py/main.py
+++ py/main.py
@@ -1,23 +1,44 @@
 import glob
-import gzip
+#import gzip
 import rdflib
 import sys
 import os
-
-ignore = ["meta"]
+import re
 
 infolder = sys.argv[1]
 outfolder = sys.argv[2]
-prefix = sys.argv[3] 
-for gz in glob.glob(infolder+"/"+prefix+"*.ttl.gz"):
+prefix = sys.argv[3]
+lang = sys.argv[4]
+if(lang==None):
+    lang = "de"
+
+# some releases around 0.8 had some literals with spaces mistakenly modelled as URIs, which breaks N-Triples serialization.
+def fixuris(s):
+    uris = re.findall(r"<[^>]*\\u00[^>]*>", s)
+    for broken in uris:
+        if(broken.startswith("<http://")):
+            continue
+        fixed = '"'+re.sub(r'[^-,.;\wäüöÖÜÄß ]',"",broken.encode('raw-unicode-escape').decode("unicode-escape"))+'"'
+        s = s.replace(broken,fixed+"@"+lang)        
+    #s = s.encode("raw-unicode-escape").decode("unicode-escape")
+    return s
+
+ignore = ["meta"]
+
+#for gz in glob.glob(infolder+"/"+prefix+"*.ttl.gz"):
+for gz in glob.glob(infolder+"/"+prefix+"*.ttl"):
     for ig in ignore:
         if(ig in gz):
             continue
-    outname = gz.replace("000001.ttl.gz",".nt").replace(prefix,"").replace(infolder,outfolder+"/"+prefix)
-    stream = gzip.open(gz,"rt")
+    print(gz)
+    #outname = gz.replace("000001.ttl.gz",".nt").replace(prefix,"").replace(infolder,outfolder+"/"+prefix)
+    outname = gz.replace("000001","").replace(".ttl",".nt").replace(prefix,"").replace(infolder,outfolder+"/"+prefix)
+    #stream = gzip.open(gz,"rt")
+    stream = open(gz,"rt")
     turtle = stream.read()
+    turtle = fixuris(turtle)
     g = rdflib.Graph()
-    g.parse(None,None,"n3",None,None,turtle)
+    g.parse(publicID="/" ,format="n3",data=turtle)
     unsorted = g.serialize(format="nt", encoding=None)
     lines = sorted(unsorted.splitlines())
     nt = b"\n".join(lines).decode("utf-8")
